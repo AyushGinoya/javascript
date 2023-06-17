df3e6b645739b2a5a67e485a71759a8f8606262e
AyushGinoya <ginoyaayush@gmail.com>
2023-06-17 10:10:59 +0530

    Change

--- learn.js
+++ learn.js
@@ -10,3 +10,32 @@ console.warn("This is warning");
 console.error("This is error");
 console.log("Language Number", 5);
 
+
+var num1 = 5;
+var num2 = 9;
+
+console.log(num1 + num2);
+console.log(5 + 9);
+
+var arr = ['a', 'b', 1, 2, "ayush", "ayushi"];
+console.log(arr);
+
+var abc;  //by defult undefined
+console.log(abc);
+
+var ab = null;
+console.log(ab);
+
+
+//object
+
+var myobj = {
+    raj: 100,
+    life_status: 'good',
+    marrige: 'not yet',
+    plan: 'next lavel',
+    city: 'Jamanagar',
+    number: 9999999999
+};
+
+console.log(myobj);
