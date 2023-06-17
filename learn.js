06cec27e526b78c7691d2d164c2b0a9d5ebef2c3
AyushGinoya <ginoyaayush@gmail.com>
2023-06-17 10:08:29 +0530

    first commit

---
+++ learn.js
@@ -0,0 +1,12 @@
+// print
+
+// console.log('Hello World');
+// document.write('hello');
+// alert('This is alert');
+
+//javascript consol API
+
+console.warn("This is warning");
+console.error("This is error");
+console.log("Language Number", 5);
+
